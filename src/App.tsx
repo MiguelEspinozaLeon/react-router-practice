1894b4d18b16332e294bf0f3db07dac6b215b442
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2023-11-01 15:34:29 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Layout } from "./components/Layout"
+import { Home } from "./components/Home"
+import { About } from "./components/About"
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Layout>
+            <Routes>
+              <Route path='/' Component={Home} />
+              <Route path='/about' Component={About} />
+            </Routes>
+        </Layout>
+      </Router>
+    </>
+  )
+}
+
+export default App
